1090a382b6cc57b5d4f6ccd81cc0049925a491f6
christo hardoin <christo.hardoin@gmail.com>
2019-10-29 11:57:20 -0400

    added text to speech, need to add it in admin dashboard

--- app/controllers/texttospeech_controller.rb
+++ app/controllers/texttospeech_controller.rb
@@ -1,12 +1,34 @@
-require './lib/api/text_speech.rb'
+require "ibm_watson/text_to_speech_v1"
+require "json"
+require "ibm_watson/authenticators"
 
 class TexttospeechController < ApplicationController
+    include IBMWatson
 
-  def create
-    # watson = Watson.new
-    synthesized_audio = Watson.create_synthesized_audio
-    data = "Hello World!"
-    send_data(synthesized_audio, :filename => "hi.wav")
-  end
+    def create
+    authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
+        apikey: 'WhqMQ53YexicoPb5pp1USHlGmB-py5ATWnUcijlyRRY-'
+      )
+      text_to_speech = IBMWatson::TextToSpeechV1.new(
+        authenticator: authenticator
+      )
+      text_to_speech.service_url = "https://gateway-syd.watsonplatform.net/text-to-speech/api"
 
+       #File.open() do |audio_file|
+      File::open("public/output.wav", "wb") do |audio_file|
+        response = text_to_speech.synthesize(
+          text: "Greetings Traveller.
+          There are currently #{Elevator.count} elevators deployed in the #{Building.count} buildings of your #{Customer.count} customers. 
+          Currently, #{Elevator.where(status: 'Inactive').count} elevators are not in Running Status and are being serviced. 
+          You currently have #{Quote.count} quotes awaiting processing. 
+          You currently have #{Lead.count} leads in your contact requests. 
+          #{Battery.count} Batteries are deployed across #{Building.joins(:address).select('addresses.city').distinct.count} cities",
+          accept: "audio/wav",
+          voice: "en-US_AllisonVoice"
+        ).result
+        audio_file << response  
+        render 'index'
+         
+      end
+    end
 end
