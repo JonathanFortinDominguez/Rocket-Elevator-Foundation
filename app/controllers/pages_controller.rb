e1bbe5433c3fc15e45a3ad1a50f8e972d8b9148f
christo hardoin <christo.hardoin@gmail.com>
2019-10-30 18:21:52 -0400

    added google map api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,6 +18,40 @@ class PagesController < ApplicationController
 
   end
 
+  def gmap
+
+    @coord_x = []
+    @coord_y = []
+    @customer_name = []
+    @number_battery = []
+    @number_column = []
+    @number_elevator = []
+    @service_name = []
+
+    for x in Building.all.each do
+
+      test = Geocoder.search(Address.find(x.id).street_number)
+      @t = test.first.coordinates
+
+      @coord_x.push(@t[0])
+      @coord_y.push(@t[1])
+
+      @customer_name.push(x.customer.full_name)
+
+      @number_battery.push(x.batteries.count)
+
+      @number_column.push(x.batteries.columns.count)
+
+      @number_elevator.push(Elevator.where(column_id: x.batteries).count)
+    
+      @service_name.push(x.customer.fullname_service_person)
+    end
+    p @coord_x
+    p @coord_y
+
+
+    
+  end
   
 
   
