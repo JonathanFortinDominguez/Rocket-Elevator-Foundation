a325b3e5b3c495e35d7f6600a84866b3cf1ed113
christo hardoin <christo.hardoin@gmail.com>
2019-10-31 20:08:44 -0400

    fixed sendgrid disabled account

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'christohardoin',
-  :password => 'CPVZPZ%?dVznT9F',
-  :domain => 'localhost:3000',
+  :user_name => ENV['SENDGRID_USER'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'rocketelevators.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
